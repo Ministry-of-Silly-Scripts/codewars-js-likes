77ceb733ed67bcb7cacfd0a623ba758cff2aa47d
Marcio Silva <marcionps@proton.me>
2023-05-23 16:57:40 -0300

    refactor to use string interpolation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const likes = (names) => {
   if (names.length === 1) {
-    return  names[0] + " likes this";
+    return  `${names[0]} likes this`;
   } 
 
   if (names.length === 2) {
-    return names[0] + " and " + names[1] + " like this";
+    return `${names[0]} and ${names[1]} like this`;
   } 
 
   if (names.length === 3) {
-    return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
   } 
 
   if (names.length > 3) {
     remainingNames = names.length - 2;
-    return names[0] + ", " + names[1] + " and " + remainingNames + " others like this";
+    return `${names[0]}, ${names[1]} and ${remainingNames} others like this`;
   } 
 
   return 'no one likes this';
