b0d0dae5bb00dcc28dc32b9d1936082d03a7a150
Marcio Silva <marcionps@proton.me>
2023-05-23 16:40:57 -0300

    added checks and updated code for inputs with 1 and 2 elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-const likes = ([]) => {
+const likes = (names) => {
+  if (names.length === 1) {
+    return names[0] + " likes this";
+  } 
+
+  if (names.length === 2) {
+    return names[0] + " and " + names[1] + " like this";
+  } 
+
   return 'no one likes this';
 };
 
--- test/index.test.js
+++ test/index.test.js
@@ -4,4 +4,12 @@ describe("likes", () => {
   test("it should return 'no one likes this' for any input", () => {
     expect(likes([])).toBe('no one likes this');
   });
+
+  test("it should return 'Peter likes this' for an input with just one element: ['Peter']", () => {
+    expect(likes(["Peter"])).toBe('Peter likes this');
+  })
+
+  test("it should return 'Jacob and Alex like this' for an input with two elements: ['Jacob', 'Alex']", () => {
+    expect(likes(["Jacob", "Alex"])).toBe('Jacob and Alex like this');
+  })
 });
